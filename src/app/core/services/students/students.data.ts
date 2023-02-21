097ef88f38c5ba5bfaf744326b4b10a42637775e
Rubenprada <rubenprada89@outlook.com>
2023-02-21 20:24:41 +0100

    FEAT: some changes

---
+++ src/app/core/services/students/students.data.ts
@@ -0,0 +1,147 @@
+
+
+
+
+
+export const students = [
+    {
+      "createdAt": "2023-02-20T16:26:08.775Z",
+      "name": "Betty Wuckert",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/458.jpg",
+      "surname": "Evangeline",
+      "id": "1"
+    },
+    {
+      "createdAt": "2023-02-20T14:01:54.394Z",
+      "name": "Beatrice Schimmel",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1017.jpg",
+      "surname": "Justus",
+      "id": "2"
+    },
+    {
+      "createdAt": "2023-02-20T09:07:00.528Z",
+      "name": "Charlene Jacobs",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/387.jpg",
+      "surname": "Jairo",
+      "id": "3"
+    },
+    {
+      "createdAt": "2023-02-20T02:25:53.083Z",
+      "name": "Howard Sawayn",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/837.jpg",
+      "surname": "Alexane",
+      "id": "4"
+    },
+    {
+      "createdAt": "2023-02-20T01:59:14.955Z",
+      "name": "Brendan Gibson",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/116.jpg",
+      "surname": "Geovanni",
+      "id": "5"
+    },
+    {
+      "createdAt": "2023-02-20T09:34:40.489Z",
+      "name": "Danny Mann",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/147.jpg",
+      "surname": "Diego",
+      "id": "6"
+    },
+    {
+      "createdAt": "2023-02-20T02:29:58.198Z",
+      "name": "Bobby Ullrich",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/333.jpg",
+      "surname": "Letha",
+      "id": "7"
+    },
+    {
+      "createdAt": "2023-02-20T02:41:11.733Z",
+      "name": "Mr. Enrique Becker MD",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/229.jpg",
+      "surname": "Ryder",
+      "id": "8"
+    },
+    {
+      "createdAt": "2023-02-20T07:03:19.867Z",
+      "name": "Lorene Maggio",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/49.jpg",
+      "surname": "Porter",
+      "id": "9"
+    },
+    {
+      "createdAt": "2023-02-19T20:31:17.783Z",
+      "name": "Micheal Kilback",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/531.jpg",
+      "surname": "Nicklaus",
+      "id": "10"
+    },
+    {
+      "createdAt": "2023-02-20T00:42:18.293Z",
+      "name": "Mr. Olive Parker",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/680.jpg",
+      "surname": "Brice",
+      "id": "11"
+    },
+    {
+      "createdAt": "2023-02-20T16:38:27.842Z",
+      "name": "Ed Bergstrom",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/905.jpg",
+      "surname": "Esperanza",
+      "id": "12"
+    },
+    {
+      "createdAt": "2023-02-19T19:15:04.071Z",
+      "name": "Erick Hudson",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/819.jpg",
+      "surname": "Randal",
+      "id": "13"
+    },
+    {
+      "createdAt": "2023-02-20T18:28:12.523Z",
+      "name": "Joe Kilback",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/118.jpg",
+      "surname": "Nella",
+      "id": "14"
+    },
+    {
+      "createdAt": "2023-02-20T14:05:48.600Z",
+      "name": "Kara Koss",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/283.jpg",
+      "surname": "Tania",
+      "id": "15"
+    },
+    {
+      "createdAt": "2023-02-19T20:57:48.916Z",
+      "name": "Becky Batz I",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/636.jpg",
+      "surname": "Ross",
+      "id": "16"
+    },
+    {
+      "createdAt": "2023-02-19T22:42:21.993Z",
+      "name": "Traci Cartwright",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/609.jpg",
+      "surname": "Quentin",
+      "id": "17"
+    },
+    {
+      "createdAt": "2023-02-20T03:43:00.784Z",
+      "name": "Kyle Prosacco",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/961.jpg",
+      "surname": "Omari",
+      "id": "18"
+    },
+    {
+      "createdAt": "2023-02-20T03:20:51.026Z",
+      "name": "Elijah Marvin",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/461.jpg",
+      "surname": "Raquel",
+      "id": "19"
+    },
+    {
+      "createdAt": "2023-02-19T22:49:46.688Z",
+      "name": "Dexter Boyle",
+      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/533.jpg",
+      "surname": "Retta",
+      "id": "20"
+    }
+  ]
